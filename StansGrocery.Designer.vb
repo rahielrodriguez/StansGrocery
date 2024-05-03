bdaae4d5cb295563fc1c5c44e229deed384eb46f
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-05-03 13:53:48 -0600

    Controls Added

--- StansGrocery.Designer.vb
+++ StansGrocery.Designer.vb
@@ -22,17 +22,119 @@ Partial Class StansGroceryForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.SearchTextBox = New System.Windows.Forms.TextBox()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.FilterComboBox = New System.Windows.Forms.ComboBox()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.FilterGroupBox = New System.Windows.Forms.GroupBox()
+        Me.FilterByAisleRadioButton = New System.Windows.Forms.RadioButton()
+        Me.FilterByCategoryRadioButton = New System.Windows.Forms.RadioButton()
+        Me.FilterGroupBox.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'SearchTextBox
+        '
+        Me.SearchTextBox.Location = New System.Drawing.Point(26, 33)
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.Size = New System.Drawing.Size(162, 22)
+        Me.SearchTextBox.TabIndex = 0
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(541, 401)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(170, 90)
+        Me.SearchButton.TabIndex = 1
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'FilterComboBox
+        '
+        Me.FilterComboBox.FormattingEnabled = True
+        Me.FilterComboBox.Location = New System.Drawing.Point(26, 81)
+        Me.FilterComboBox.Name = "FilterComboBox"
+        Me.FilterComboBox.Size = New System.Drawing.Size(162, 24)
+        Me.FilterComboBox.TabIndex = 2
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 16
+        Me.DisplayListBox.Location = New System.Drawing.Point(26, 130)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(503, 356)
+        Me.DisplayListBox.TabIndex = 3
+        '
+        'DisplayLabel
+        '
+        Me.DisplayLabel.AutoSize = True
+        Me.DisplayLabel.Location = New System.Drawing.Point(216, 83)
+        Me.DisplayLabel.Name = "DisplayLabel"
+        Me.DisplayLabel.Size = New System.Drawing.Size(0, 16)
+        Me.DisplayLabel.TabIndex = 4
+        '
+        'FilterGroupBox
+        '
+        Me.FilterGroupBox.Controls.Add(Me.FilterByCategoryRadioButton)
+        Me.FilterGroupBox.Controls.Add(Me.FilterByAisleRadioButton)
+        Me.FilterGroupBox.Location = New System.Drawing.Point(541, 130)
+        Me.FilterGroupBox.Name = "FilterGroupBox"
+        Me.FilterGroupBox.Size = New System.Drawing.Size(170, 104)
+        Me.FilterGroupBox.TabIndex = 5
+        Me.FilterGroupBox.TabStop = False
+        Me.FilterGroupBox.Text = "FilterGroupBox"
+        '
+        'FilterByAisleRadioButton
+        '
+        Me.FilterByAisleRadioButton.AutoSize = True
+        Me.FilterByAisleRadioButton.Location = New System.Drawing.Point(10, 27)
+        Me.FilterByAisleRadioButton.Name = "FilterByAisleRadioButton"
+        Me.FilterByAisleRadioButton.Size = New System.Drawing.Size(103, 20)
+        Me.FilterByAisleRadioButton.TabIndex = 0
+        Me.FilterByAisleRadioButton.TabStop = True
+        Me.FilterByAisleRadioButton.Text = "FilterByAisle"
+        Me.FilterByAisleRadioButton.UseVisualStyleBackColor = True
+        '
+        'FilterByCategoryRadioButton
+        '
+        Me.FilterByCategoryRadioButton.AutoSize = True
+        Me.FilterByCategoryRadioButton.Location = New System.Drawing.Point(10, 72)
+        Me.FilterByCategoryRadioButton.Name = "FilterByCategoryRadioButton"
+        Me.FilterByCategoryRadioButton.Size = New System.Drawing.Size(128, 20)
+        Me.FilterByCategoryRadioButton.TabIndex = 1
+        Me.FilterByCategoryRadioButton.TabStop = True
+        Me.FilterByCategoryRadioButton.Text = "FilterByCategory"
+        Me.FilterByCategoryRadioButton.UseVisualStyleBackColor = True
         '
         'StansGroceryForm
         '
+        Me.AcceptButton = Me.SearchButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(732, 503)
+        Me.Controls.Add(Me.FilterGroupBox)
+        Me.Controls.Add(Me.DisplayLabel)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.FilterComboBox)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.SearchTextBox)
         Me.Name = "StansGroceryForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Stan's Grocery"
+        Me.FilterGroupBox.ResumeLayout(False)
+        Me.FilterGroupBox.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents SearchTextBox As TextBox
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents FilterComboBox As ComboBox
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents DisplayLabel As Label
+    Friend WithEvents FilterGroupBox As GroupBox
+    Friend WithEvents FilterByCategoryRadioButton As RadioButton
+    Friend WithEvents FilterByAisleRadioButton As RadioButton
 End Class
--- StansGrocery.vb
+++ StansGrocery.vb
@@ -2,4 +2,8 @@
     Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+
+    End Sub
 End Class
